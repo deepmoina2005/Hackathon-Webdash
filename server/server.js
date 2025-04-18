5a9e6ef73c48be6c62ea02dfd27ed65ea166354f
deepmoina2005 <deepmoinaboruah7@gmail.com>
2025-04-18 18:10:27 +0530

    update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import addressRouter from "./routes/addressRoute.js";
 import orderRouter from "./routes/orderRoute.js";
-import { stripeWebhooks } from "./controllers/orderController.js";
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -18,8 +17,6 @@ await connectDB();
 await connectCloudinary();
 
 const allowedOrigins = ['http://localhost:5173']
-
-app.post('/stripe', express.raw({type: 'application/json'}, stripeWebhooks))
 
 app.use(express.json());
 app.use(cookieParser());
